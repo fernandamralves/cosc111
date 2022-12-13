c4060eaab2ea00f9706bb45fe15f64f5fe840198
Fernanda Alves <72361088+fernandamralves@users.noreply.github.com>
2022-12-13 11:18:50 -0800

    Add files via upload

---
+++ Video Game - Side Scroller - Star Wars Theme/Cell.java
@@ -0,0 +1,28 @@
+package GameLab8;
+
+public class Cell {
+    private Color color;
+    private String imageFileName;
+
+    public Cell() {
+        color = new Color(0, 0, 51);
+        //color = "Space.gif";
+        imageFileName = null;
+    }
+
+    public void setColor(Color c) {
+        color = c;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public String getImageFileName() {
+        return imageFileName;
+    }
+
+    public void setImageFileName(String fileName) {
+        imageFileName = fileName;
+    }
+}
